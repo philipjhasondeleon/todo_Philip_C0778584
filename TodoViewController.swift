814e2da3f367ff6f9272036b57ecc6c50ad4f1af
Philip <jhasondeleon@gmail.com>
2020-06-29 00:34:56 -0400

    todoViewController created

---
+++ TodoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodoViewController.swift
+//  todo_Philip_C0778584
+//
+//  Created by user175490 on 6/29/20.
+//  Copyright © 2020 user175490. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- todo_Philip_C0778584/ViewController.swift
+++ todo_Philip_C0778584/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+import UserNotifications
 
 class ViewController: UIViewController {
 
